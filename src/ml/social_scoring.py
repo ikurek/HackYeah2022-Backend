c0ef1c070dd7da9e09519247c9697c32ad60e7bb
Kamil Jędryczek <kamillo.j@gmail.com>
2022-11-20 03:53:42 +0100

    fixing math logarithm

--- src/ml/social_scoring.py
+++ src/ml/social_scoring.py
@@ -5,7 +5,7 @@ LIKE_WEIGHT = 2
 LOGARITHM_MAX_POWER = 4
 def calculate_social_scoring(fraud_score: float, retweet_count: int, like_count: int):
     influence = RETWEET_WEIGHT * retweet_count + LIKE_WEIGHT * like_count
-    influence = math.ln(influence)
+    influence = math.log(influence)
     influence = min(influence, LOGARITHM_MAX_POWER)
 
     return math.ceil(fraud_score * influence / 4)
