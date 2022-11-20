63ccfcc0ce299fd2334534fe22477115afdd3bb9
Kamil Jędryczek <kamillo.j@gmail.com>
2022-11-20 03:50:24 +0100

    changes of scores in pipeline

--- src/ml/pipeline.py
+++ src/ml/pipeline.py
@@ -2,9 +2,10 @@ from typing import Tuple, List, Optional
 
 from src.ml.language import get_language, translate, LANGUAGE_REVERSE_MAPPER, polish_text_to_embeddings, PL, EN
 from src.ml.one_shot_classifier import evaluate_multiple_hypothesis
+from src.ml.social_scoring import calculate_social_scoring
 
 
-def process_tweet(text: str) -> Optional[Tuple[List[float], float]]:
+def process_tweet(text: str, retweet_count: int, like_count: int) -> Optional[Tuple[List[float], float]]:
     """
     Translate tweet, calculate embeddings by HerBERT and score scam by one zero shot model.
 
@@ -16,5 +17,6 @@ def process_tweet(text: str) -> Optional[Tuple[List[float], float]]:
             text_language: text,
             LANGUAGE_REVERSE_MAPPER[text_language]: translate(text, text_language)
         }
-        return polish_text_to_embeddings(texts[PL]), evaluate_multiple_hypothesis(texts[EN])
+        fraud_score = evaluate_multiple_hypothesis(texts[EN])
+        return polish_text_to_embeddings(texts[PL]), fraud_score, calculate_social_scoring(fraud_score, retweet_count, like_count)
     return None
